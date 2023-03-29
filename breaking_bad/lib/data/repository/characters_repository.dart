9122d3899fb607de7c1b6f09b09756d7e443d34c
Buda Samir <Abdulrahman.samir.attia.elseed.96@gmail.com>
2023-03-29 22:26:55 +0200

    cubit Implementation

--- breaking_bad/lib/data/repository/characters_repository.dart
+++ breaking_bad/lib/data/repository/characters_repository.dart
@@ -6,7 +6,7 @@ class CharactersRepository {
   final CharactersAPI charactersAPI;
   CharactersRepository({required this.charactersAPI});
 
-  Future<List<dynamic>> getAllCharacter() async {
+  Future<List<Character>> getAllCharacter() async {
     final character = await charactersAPI.getAllCharacter();
     return character.map((character) => Character.formJson(character)).toList();
   }

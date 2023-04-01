41234f95757bdd3c3cb343bedadb35b43f6e2b83
Buda Samir <Abdulrahman.samir.attia.elseed.96@gmail.com>
2023-04-01 03:46:49 +0200

    bloc builder implementation

--- breaking_bad/lib/presentation/screens/characters_screen.dart
+++ breaking_bad/lib/presentation/screens/characters_screen.dart
@@ -1,10 +1,79 @@
+import 'package:breaking_bad/constants/colors.dart';
+import 'package:breaking_bad/data/models/characters.dart';
+import 'package:breaking_bad/domain/cubit/characters_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class CharactersScreen extends StatelessWidget {
+class CharactersScreen extends StatefulWidget {
   const CharactersScreen({super.key});
 
   @override
+  State<CharactersScreen> createState() => _CharactersScreenState();
+}
+
+class _CharactersScreenState extends State<CharactersScreen> {
+  late List<Character> allCharacter;
+
+  @override
+  void initState() {
+    super.initState();
+    allCharacter = BlocProvider.of<CharactersCubit>(context).getAllCharacters();
+  }
+
+  Widget buildBlocWidget() {
+    return BlocBuilder<CharactersCubit, CharactersState>(
+        builder: (context, state) {
+      if (state is CharactersLoaded) {
+        allCharacter = (state).characters;
+        return buildLoadedListWidgets();
+      } else {
+        return showLoadingIndicator();
+      }
+    });
+  }
+
+  Widget buildLoadedListWidgets() {
+    return SingleChildScrollView(
+      child: Container(
+        color: MyColors.grey,
+        child: Column(
+          children: [
+            buildCharacterList(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildCharacterList() {
+    return GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, childAspectRatio: 2 / 3, crossAxisSpacing: 3),
+        shrinkWrap: true,
+        physics: const ClampingScrollPhysics(),
+        padding: EdgeInsets.zero,
+        itemCount: allCharacter.length,
+        itemBuilder: (BuildContext context, int index) {
+          return;
+        });
+  }
+
+  Center showLoadingIndicator() {
+    return const Center(
+      child: CircularProgressIndicator(
+        color: MyColors.yellow,
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: MyColors.yellow,
+        title: const Text("Characters", style: TextStyle(color: MyColors.grey)),
+      ),
+      body: buildBlocWidget(),
+    );
   }
 }

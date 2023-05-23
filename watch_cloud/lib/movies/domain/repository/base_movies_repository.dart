80679a807a33b14a2d71124aca2ead61c5e19695
Buda Samir <Abdulrahman.samir.attia.elseed.96@gmail.com>
2023-05-23 11:22:32 +0300

    1- created new movie app project, 2- created the project setup,3- implement Entities(Domain) file: [Movies Entity], 4- implement Repository(Domain) file: [BaseMoviesRepository] and Methods [getNowPlayingMovies,getPopularMovies,getTopRatedMovies], 5- imp UsesCases(Domian) files : [getNowPlayingMovies,getPopularMovies,getTopRatedMovies] 5- imp UsesCase(Domain) file : getNowPlayingMovies

---
+++ watch_cloud/lib/movies/domain/repository/base_movies_repository.dart
@@ -0,0 +1,7 @@
+import 'package:watch_cloud/movies/domain/entities/movies.dart';
+
+abstract class BaseMoviesRepository {
+  Future<List<Movies>> getNowPlayingMovies();
+  Future<List<Movies>> getPopularMovies();
+  Future<List<Movies>> getTopRatedMovies();
+}
---
+++ watch_cloud/lib/movies/domain/usecases/get_now_playing_movies_usecase.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:watch_cloud/movies/domain/entities/movies.dart';
+import 'package:watch_cloud/movies/domain/repository/base_movies_repository.dart';
+
+class GetNowPlayingMoviesUseCase {
+  final BaseMoviesRepository baseMoviesRepository;
+  GetNowPlayingMoviesUseCase({required this.baseMoviesRepository});
+
+  Future<List<Movies>> execute() {
+    return baseMoviesRepository.getNowPlayingMovies();
+  }
+}

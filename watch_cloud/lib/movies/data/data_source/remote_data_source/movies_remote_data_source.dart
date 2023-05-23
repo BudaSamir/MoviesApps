30c76deba3a1b293ec48abc483d90913416b2c3e
Buda Samir <Abdulrahman.samir.attia.elseed.96@gmail.com>
2023-05-23 16:25:12 +0300

    created DataSource(data) file[movies_remote_data_source]: created MovieRemoteDataSource class && Imp it

---
+++ watch_cloud/lib/movies/data/data_source/remote_data_source/movies_remote_data_source.dart
@@ -0,0 +1,15 @@
+import 'package:dio/dio.dart';
+import 'package:watch_cloud/movies/data/models/movies_model.dart';
+
+class MovieRemoteDataSource {
+  getNowPlayingMovies() async {
+    final response = await Dio().get(
+        "https://api.themoviedb.org/3/movie/now_playing?api_key=15c5a86f88b357bac4367a856db0e6f3");
+    if (response.statusCode == 200) {
+      return List<MoviesModel>.from(
+          (response.data["results"] as List).map((json) => MoviesModel.fromJson(json)));
+    } else{
+      throw Exception("API Error");
+    }
+  }
+}
